0bef31ddf180a49287ae8a0af53f1f0d540a6e23
Md.Asaduzzaman Rial <mdrialm@gmail.com>
2025-11-02 03:26:55 +0000

    #

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 151515151515
+let accountEmail = "mdrial@gmail.com"
+var accountPassword = "12df34"
+accountCity = "joypurhat"
+
+accountEmail = "farjana@gmail.com"
+accountPassword = "123ddld"
+accountCity = "dhaka"
+accountState = "nok"
+
+accountEmail = "lok@gmail.com"
+accountPassword = "202025"
+accountCity = "nator"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+console.table([accountEmail, accountPassword, accountCity])
